7d2b64b1dd8dd9b29a50d856e4dd940a5942b7b0
SWTgrp22 <78541817+SWTgrp22@users.noreply.github.com>
2021-02-18 13:02:38 +0100

    Add files via upload

---
+++ ECSLegacy/ECSLegacy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ECSLegacy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ecs = new ECS(28);
+
+            ecs.Regulate();
+
+            ecs.SetThreshold(20);
+
+            ecs.Regulate();
+
+            ecs.SetThreshold(26);
+
+            ecs.Regulate();
+
+        }
+    }
+}
